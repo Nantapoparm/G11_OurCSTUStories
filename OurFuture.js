1cd75311f6cc70fdb5080a2c22dd30a923bb1aa2
Nadhsikarn <Nadhsikarn.jon@dome.tu.ac.th>
2024-12-01 19:03:16 +0700

    fix typo

--- OurFuture.js
+++ OurFuture.js
@@ -1,7 +1,3 @@
-// ฟังก์ชันเพื่อคลิกแล้วแสดงข้อความ
-function toggleText(card) {
-    card.classList.toggle('clicked');
-}
 //ฟังก์ชัน toggleMenu
 function toggleMenu(){
     const menu  = document.querySelector("nav ul");
