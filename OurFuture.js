d1500d92b22bfe3dd83c4d86b012cb71521b73dd
Printan <printan.nun@dome.tu.ac.th>
2024-12-01 18:01:52 +0700

    Change Picture in html and add function in OurFuture.js

--- OurFuture.js
+++ OurFuture.js
@@ -2,3 +2,8 @@
 function toggleText(card) {
     card.classList.toggle('clicked');
 }
+//ฟังก์ชัน toggleMenu
+function toggleMenu(){
+    const menu  = document.querySelector("nav ul");
+    menu.classList.toggle("active");
+}
