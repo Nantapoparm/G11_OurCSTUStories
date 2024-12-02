60e08299db86c9afd0e56fe874d8f6c5d60dae20
Nadhsikarn <Nadhsikarn.jon@dome.tu.ac.th>
2024-12-02 21:10:01 +0700

    change inline javascript to external javascript for OurFuture

--- OurFuture.js
+++ OurFuture.js
@@ -1,9 +1,15 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click',function() {
+        toggleText(this);
+    })
+})
+// ฟังก์ชันเพื่อคลิกแล้วแสดงข้อความ
+function toggleText(card) {
+    card.classList.toggle('clicked');
+}
+
 //ฟังก์ชัน toggleMenu
 function toggleMenu(){
     const menu  = document.querySelector("nav ul");
     menu.classList.toggle("active");
-}
-// ฟังก์ชันเพื่อคลิกแล้วแสดงข้อความ
-function toggleText(card) {
-    card.classList.toggle('clicked');
 }
